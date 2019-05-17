bd72466af7bf2d93a6e962c3228f195f1e73731c
sunhaolin <sunhaolin@hotoa.com>
2019-05-17 18:14:53 +0800

    graphql和odata接口集成进steedos-api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 let objectql = require("@steedos/objectql");
 let express = require('express');
-let jwtRouter = require('@steedos/auth').jwtRouter;
+let graphqlHTTP = require('express-graphql');
+let steedosAuth = require('@steedos/auth');
+let jwtRouter = steedosAuth.jwtRouter;
 let _ = require('underscore');
+let Cookies = require("cookies");
 
 let app = express();
 
@@ -13,5 +16,37 @@ _.each(schemaConfig.datasources, (datasource, name) => {
 })
 
 app.use('/api-v2', jwtRouter);
+
+let ODataRouter = require('@steedos/core').ODataRouter;
+_.each(steedosSchema.getDataSources(), (datasource, name) => {
+    app.use("/api-v2/odata/v4", ODataRouter);
+})
+
+
+let graphqlRouter = express.Router();
+graphqlRouter.use((req, res, next) => {
+    let cookies = new Cookies(req, res);
+    let authToken: string = req.headers['x-auth-token'] || cookies.get("X-Auth-Token");
+    if (!authToken && req.headers.authorization && req.headers.authorization.split(' ')[0] == 'Bearer') {
+        authToken = req.headers.authorization.split(' ')[1]
+    }
+
+    let user = null
+    if (authToken)
+        user = steedosAuth.getSession(authToken);
+
+    if (user)
+        next();
+    else
+        res.status(401).send({ errors: [{ 'message': 'You must be logged in to do this.' }] });
+})
+_.each(steedosSchema.getDataSources(), (datasource, name) => {
+    graphqlRouter.use("/#{name}", graphqlHTTP({
+        schema: datasource.buildGraphQLSchema(),
+        graphiql: true
+    }))
+})
+
+app.use('/api-v2/graphql', graphqlRouter);
 
 app.listen(process.env.PORT || 3333);
--- steedos.config.js
+++ steedos.config.js
@@ -1,14 +1,17 @@
 let path = require('path');
 let objectFilesPath = path.resolve(path.join(process.cwd(), './objects'));
+let standardObjectsDir = path.dirname(require.resolve("@steedos/standard-objects"))
 
 console.log('objectFilesPath: ', objectFilesPath);
+console.log('standardObjectsDir: ', standardObjectsDir);
 
 module.exports = {
   datasources: {
     default: {
       driver: 'mongo',
-      url: 'mongodb://172.17.5.127:27017/steedos',
-      objectFiles: [objectFilesPath]
+      url: 'mongodb://127.0.0.1:27017/steedos',
+      // url: 'mongodb://172.17.5.127:27017/steedos',
+      objectFiles: [objectFilesPath, standardObjectsDir]
     }
   }
 }
